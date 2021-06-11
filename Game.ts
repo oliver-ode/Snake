e9a213e9cdc2f4e976ec05977c27530b72fc4db4
Oliver Odendaal <oliverode@gmail.com>
2021-06-11 16:58:17 -0400

    Basic board drawing

--- Game.ts
+++ Game.ts
@@ -5,18 +5,22 @@ class Game {
     private readonly WIDTH = 40;
     private readonly HEIGHT = 40;
     
-    // private board: string[][];
+    private board: string[][];
+    private scale: number;
 
     public constructor() {
         this.canvas = document.querySelector('canvas') as HTMLCanvasElement;
         this.context = this.canvas.getContext('2d');
 
-        // for (let y = 0; y < this.HEIGHT; y++) {
-        //     this.board[y] = [];
-        //     for (let x = 0; x < this.WIDTH; x++){
-        //         this.board[y][x] = "empty";
-        //     }
-        // }
+        this.scale = this.canvas.height/this.HEIGHT;
+
+        this.board = [];
+        for (let y = 0; y < this.HEIGHT; y++) {
+            this.board[y] = [];
+            for (let x = 0; x < this.WIDTH; x++){
+                this.board[y][x] = "empty";
+            }
+        }
 
         this.draw = this.draw.bind(this);
     }
@@ -24,12 +28,22 @@ class Game {
     public draw() {
         const ctx = this.context;
         const c = this.canvas;
+        const s = this.scale;
         ctx.clearRect(0, 0, c.width, c.height);
 
         ctx.fillStyle = "#000000";
         ctx.fillRect(0, 0, c.width, c.height);
+
         
         ctx.fillStyle = "#3bb1d9";
-        ctx.fillRect(10, 10, 10, 10);
+        for (let y = 0; y < this.HEIGHT; y++) {
+            for (let x = 0; x < this.WIDTH; x++){
+                ctx.rect(x*s, y*s, s, s);
+                ctx.fill()
+                ctx.lineWidth = 1;
+                ctx.strokeStyle = "black";
+                ctx.stroke();
+            }
+        }
     }
 }
